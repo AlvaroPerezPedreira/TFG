cfb8f31dee4955e1b84019b0dff78e28fd4993ae
AlvaroPerezPedreira <alvaro.perez.pedreira@udc.es>
2024-11-27 17:12:06 +0100

    theme button

--- frontend/src/components/GlobalComponents/ThemeComponent.jsx
+++ frontend/src/components/GlobalComponents/ThemeComponent.jsx
@@ -1,25 +1,34 @@
 import React from "react";
-import { MoonIcon, SunIcon } from "../../icons/ThemeIcons";
-import {Switch} from "@nextui-org/switch";
+import {
+  MoonIcon,
+  SunIcon,
+  NbSunIcon,
+  NbMoonIcon,
+} from "../../icons/ThemeIcons";
+import { Switch } from "@nextui-org/switch";
 import { useThemeContext } from "../../context/ThemeContext";
+import { Button } from "@nextui-org/button";
 
 export default function ThemeComponent() {
-    const { color, dark, setDark } = useThemeContext();
+  const { color, dark, setDark } = useThemeContext();
 
-    const handleMode = () => {
-      setDark((prev) => !prev);
-    };
+  const handleMode = () => {
+    setDark((prev) => !prev);
+  };
 
   return (
     <>
-    <Switch
-      defaultSelected
-      size="md"
-      color="default"
-      startContent={<SunIcon />}
-      endContent={<MoonIcon />}
-      onChange={handleMode}
-    />
+      <Button isIconOnly onClick={handleMode} variant="" size="sm">
+        {dark ? <NbMoonIcon /> : <NbSunIcon />}
+      </Button>
+      {/* <Switch
+        defaultSelected
+        size="md"
+        color="default"
+        startContent={<SunIcon />}
+        endContent={<MoonIcon />}
+        onChange={handleMode}
+      /> */}
     </>
   );
 }
